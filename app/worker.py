88219a8e84628550a461d5e19b32a5ab2cbc559e
Nahom <nahiyo86@gmail.com>
2026-04-23 20:23:01 +0300

    feat: worker to execute jobs on queue and upload to storage on success

---
+++ app/worker.py
@@ -0,0 +1,53 @@
+# app/worker.py
+
+import yt_dlp
+import subprocess
+from app.jobs import update_job, job_queue
+
+
+def download(url, output):
+    ydl_opts = {
+        "format": "bestvideo+bestaudio/best",
+        "outtmpl": output + ".%(ext)s",
+        "merge_output_format": "mp4",
+        "quiet": True,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    return output + ".mp4"
+
+
+def cut(input_file, start, duration, output):
+    cmd = [
+        "ffmpeg",
+        "-ss", start,
+        "-i", input_file,
+        "-t", str(duration),
+        "-c:v", "libx264",
+        "-c:a", "aac",
+        "-preset", "veryfast",
+        "-movflags", "+faststart",
+        output
+    ]
+
+    subprocess.run(cmd, check=True)
+
+
+def worker_loop():
+    while True:
+        job_id, data = job_queue.get()
+
+        try:
+            update_job(job_id, "processing")
+
+            input_file = download(data["url"], f"outputs/{job_id}")
+            output_file = f"outputs/{job_id}_clip.mp4"
+
+            cut(input_file, data["start"], data["duration"], output_file)
+
+            update_job(job_id, "done", output_file)
+
+        except Exception as e:
+            update_job(job_id, "failed", str(e))
